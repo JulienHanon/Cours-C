72f8337dd57da849e51e60d7bb43a270c431acfa
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2021-11-15 12:04:39 +0100

    ajout commentaire

--- RappelProgrammation/main.cpp
+++ RappelProgrammation/main.cpp
@@ -5,18 +5,18 @@ int main(){
 
     cout << "Bienvenue au juste prix " << endl;
 
-    auto aDeviner(0);
-    auto nombre(0);
+    auto aDeviner(0); //variable contenant le chiffre que l'utilisateur doit deviner
+    auto nombre(0); // variable contenant le chiffre que l'utilisateur rentre
 
     cin >> aDeviner;
 
     cout << "Devinez le nombre : ";
     cin >> nombre;
 
-    if (nombre == aDeviner){
-        cout << "Vous avez gagné !";
+    if (nombre == aDeviner){ // si le nombre rentrer est egal au nombre a deviner affiche vous avez gagne
+        cout << "Vous avez gagne";
     }
-    else if (nombre < aDeviner || nombre > aDeviner){
+    else { // si le nombre rentrer est inferieur ou supérieur au nombre a deviner affiche vous avez perdu
         cout << "Vous avez perdu";
     }
 
