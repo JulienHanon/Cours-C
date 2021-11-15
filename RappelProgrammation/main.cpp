db6f0680e0406da9ac648c8feb8064d5f001d409
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2021-11-15 14:04:58 +0100

    ajout boucle range for

--- RappelProgrammation/main.cpp
+++ RappelProgrammation/main.cpp
@@ -5,49 +5,41 @@ int main(){
 
     cout << "Bienvenue au juste prix " << endl;
 
-    auto aDeviner(0);
+
+    int aDeviner[3] = {25, 34, 34};
+
+    for(int i : aDeviner){
+
+
     auto proposition(0);
-    int i = 0;
+    int nombreTentative = 0;
 
-
-
-
-    for (int j =  1; j < 4; j++){
-        cout <<"Partie numero " << j <<endl;
-        cout <<"Rentrez le chiffre a deviner :";
-        cin >> aDeviner;
-        cout << "Devinez le nombre : ";
-        cin >> proposition;
         do {
+            cout << "Devinez le nombre : ";
+            cin >> proposition;
 
-             if (proposition < aDeviner){
-                i++;
+            if(proposition < 9999 || proposition > 0){
+             if (proposition < i){
+                nombreTentative++;
                 cout <<"C'est plus !"<<endl << "Devinez le nombre :";
                 cin >> proposition;
-                if(proposition > 9999 || proposition < 0){
-                    cout << "Veuillez rentrer un nombre compris entre 10000 et 0 :";
-                    cin >> proposition;
-
-                }
-
             }
-            else if (proposition > aDeviner){
-                i++;
+            else if (proposition > i){
+                nombreTentative++;
                 cout <<"C'est moins !" <<endl<<"Devinez le nombre :";
                 cin >> proposition;
-                if(proposition > 9999 || proposition < 0){
-                    cout << "Veuillez rentrer un nombre compris entre 10000 et 0 :";
-                    cin >> proposition;
-
-                }
+            }
+            }
+            else{
+               cout<< "Veuillez rentrez un chiffre situer entre 0 et 10000";
             }
 
-        }while (proposition != aDeviner);
+        }while (proposition != i);
 
         i++;
-        cout << "Vous avez gagne" << endl<< "Nombre de tentatives : " << i <<endl;
+        cout << "Vous avez gagne" << endl<< "Nombre de tentatives : " << nombreTentative <<endl;
         proposition = 0;
-        i = 0;
+        nombreTentative = 0;
     }
 
     cout << "Partie termine"<<endl;
