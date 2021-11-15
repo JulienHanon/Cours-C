2f4e9452057972bdac613aaacb43daa904400da1
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2021-11-15 12:01:58 +0100

    Condition perdu/gagner

--- RappelProgrammation/main.cpp
+++ RappelProgrammation/main.cpp
@@ -6,11 +6,23 @@ int main(){
     cout << "Bienvenue au juste prix " << endl;
 
     auto aDeviner(0);
+    auto nombre(0);
 
     cin >> aDeviner;
 
-    cout << "Vous avez rentrez : "<< aDeviner;
+    cout << "Devinez le nombre : ";
+    cin >> nombre;
 
+    if (nombre == aDeviner){
+        cout << "Vous avez gagné !";
+    }
+    else if (nombre < aDeviner || nombre > aDeviner){
+        cout << "Vous avez perdu";
+    }
+
+
+
+    return 0;
 
 
 }
