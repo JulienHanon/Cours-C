0443c73c7e5611cf9e5bee1a61a2d1372b156286
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2021-11-15 14:29:42 +0100

    ajout switch

--- RappelProgrammation/main.cpp
+++ RappelProgrammation/main.cpp
@@ -3,32 +3,39 @@
 using namespace std;
 int main(){
 
-    cout << "Bienvenue au juste prix " << endl;
-
 
     int aDeviner[3] = {25, 34, 34};
+    int n = 0;
+    cout << "Selectionner un jeu :"<< "1 - Juste Prix";
+    cin >> n;
 
-    for(int i : aDeviner){
+       switch (n)
+    {
+
+    case 1:
+        cout << "Bienvenue au juste prix " << endl;
+            for(int i : aDeviner){
 
 
     auto proposition(0);
     int nombreTentative = 0;
 
+
         do {
+
             cout << "Devinez le nombre : ";
             cin >> proposition;
 
-            if(proposition < 9999 || proposition > 0){
-             if (proposition < i){
-                nombreTentative++;
-                cout <<"C'est plus !"<<endl << "Devinez le nombre :";
-                cin >> proposition;
-            }
-            else if (proposition > i){
-                nombreTentative++;
-                cout <<"C'est moins !" <<endl<<"Devinez le nombre :";
-                cin >> proposition;
-            }
+            if(proposition < 10000 || proposition > 0){
+
+                if (proposition < i){
+                    nombreTentative++;
+                    cout <<"C'est plus !"<<endl;
+                }
+                else if (proposition > i){
+                    nombreTentative++;
+                    cout <<"C'est moins !" <<endl;
+                }
             }
             else{
                cout<< "Veuillez rentrez un chiffre situer entre 0 et 10000";
@@ -36,13 +43,20 @@ int main(){
 
         }while (proposition != i);
 
-        i++;
+
         cout << "Vous avez gagne" << endl<< "Nombre de tentatives : " << nombreTentative <<endl;
         proposition = 0;
         nombreTentative = 0;
     }
 
     cout << "Partie termine"<<endl;
+
+    case 2:
+       cout <<"non";
+        break;
+    }
+
+
 
 
     return 0;
