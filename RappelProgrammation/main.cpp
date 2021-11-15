919a6c7c23b1b95c86732f54f7bc44c108fe59fa
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2021-11-15 12:43:40 +0100

    Ajout nombre de partie (3)

--- RappelProgrammation/main.cpp
+++ RappelProgrammation/main.cpp
@@ -5,44 +5,54 @@ int main(){
 
     cout << "Bienvenue au juste prix " << endl;
 
-    auto aDeviner(2456); //variable contenant le chiffre que l'utilisateur doit deviner
-    auto nombre(0); // variable contenant le chiffre que l'utilisateur rentre
+    auto aDeviner(0);
+    auto nombre(0);
     int i = 0;
 
-    //cout <<"Rentrez le chiffre a deviner :";
-
-    //cin >> aDeviner;
-
-    cout << "Devinez le nombre : ";
-    cin >> nombre;
-
-    do {
-        if (nombre == aDeviner){
-            i++;
-            cout << "Vous avez gagne";
-            cout << "Nombre de tentatives : ";
-            cout << i;
-        }
-        else if (nombre < aDeviner){
-            i++;
-            cout <<"C'est plus !"<<endl;
-            cout << "Devinez le nombre : ";
-            cin >> nombre;
-        }
-        else if (nombre > aDeviner){
-            i++;
-            cout <<"C'est moins !" <<endl;
-            cout << "Devinez le nombre : ";
-            cin >> nombre;
-        }
-
-    }while (nombre != aDeviner);
 
 
-    i++;
-    cout << "Vous avez gagne" << endl;
-    cout << "Nombre de tentatives : ";
-    cout << i;
+
+    for (int j = 0; j < 3; j++){
+        cout <<"Rentrez le chiffre a deviner :";
+        cin >> aDeviner;
+        cout << "Devinez le nombre : ";
+        cin >> nombre;
+        do {
+
+             if (nombre < aDeviner){
+                i++;
+                cout <<"C'est plus !"<<endl;
+                cout << "Devinez le nombre : ";
+                cin >> nombre;
+                if(nombre > 10000){
+                    cout << " Veuillez rentrer un nombre inferieur a 10000 :";
+                    cin >> nombre;
+
+                }
+
+            }
+            else if (nombre > aDeviner){
+                i++;
+                cout <<"C'est moins !" <<endl;
+                cout << "Devinez le nombre : ";
+                cin >> nombre;
+                if(nombre > 10000){
+                    cout << " Veuillez rentrer un nombre inferieur a 10000 :"<<endl;
+                    cin >> nombre;
+
+                }
+            }
+
+        }while (nombre != aDeviner);
+
+        i++;
+        cout << "Vous avez gagne" << endl;
+        cout << "Nombre de tentatives : " << i <<endl;
+        nombre = 0;
+        i = 0;
+    }
+
+    cout << "Partie termine"<<endl;
 
 
     return 0;
