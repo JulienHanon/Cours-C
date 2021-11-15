ebbd444fbd91e5386428687cb3487f5351836453
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2021-11-15 13:08:57 +0100

    changement de nom des variables

--- RappelProgrammation/main.cpp
+++ RappelProgrammation/main.cpp
@@ -6,7 +6,7 @@ int main(){
     cout << "Bienvenue au juste prix " << endl;
 
     auto aDeviner(0);
-    auto nombre(0);
+    auto proposition(0);
     int i = 0;
 
 
@@ -17,39 +17,39 @@ int main(){
         cout <<"Rentrez le chiffre a deviner :";
         cin >> aDeviner;
         cout << "Devinez le nombre : ";
-        cin >> nombre;
+        cin >> proposition;
         do {
 
-             if (nombre < aDeviner){
+             if (proposition < aDeviner){
                 i++;
                 cout <<"C'est plus !"<<endl;
                 cout << "Devinez le nombre : ";
-                cin >> nombre;
-                if(nombre > 9999){
-                    cout << "Veuillez rentrer un nombre inferieur a 10000 :";
-                    cin >> nombre;
+                cin >> proposition;
+                if(proposition > 9999 || proposition < 0){
+                    cout << "Veuillez rentrer un nombre compris entre 10000 et 0 :";
+                    cin >> proposition;
 
                 }
 
             }
-            else if (nombre > aDeviner){
+            else if (proposition > aDeviner){
                 i++;
                 cout <<"C'est moins !" <<endl;
                 cout << "Devinez le nombre : ";
-                cin >> nombre;
-                if(nombre > 9999){
-                    cout << "Veuillez rentrer un nombre inferieur a 10000 :"<<endl;
-                    cin >> nombre;
+                cin >> proposition;
+                if(proposition > 9999 || proposition < 0){
+                    cout << "Veuillez rentrer un nombre compris entre 10000 et 0 :";
+                    cin >> proposition;
 
                 }
             }
 
-        }while (nombre != aDeviner);
+        }while (proposition != aDeviner);
 
         i++;
         cout << "Vous avez gagne" << endl;
         cout << "Nombre de tentatives : " << i <<endl;
-        nombre = 0;
+        proposition = 0;
         i = 0;
     }
 
