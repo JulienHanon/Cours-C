bbb7e6cdd6248f3dbdb14f7bbbdf8ba3ccaa9786
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2021-11-16 15:49:19 +0100

    ajout d'un tableau avec la liste des lettres + boucles pour retirer les lettres deja utiliser de cette derniere

--- JeuDuPendu/main.cpp
+++ JeuDuPendu/main.cpp
@@ -5,6 +5,7 @@
 #include "Constantes.h"
 
 
+
 using namespace std;
 
 int main()
@@ -13,16 +14,69 @@ int main()
     cout << "Bienvenue dans le jeu du pendu !"<<endl;
 
 
-    int TailleMot(0), nbErreur(0);
-    string MotATrouver;
-    char lettreAEssayer;
+    int TailleMot(0), nbErreur(0), NbLettreATrouver(0);
+    string MotATrouver, motTravail;
+    char LettreUser;
+    bool lettreDejaEssayee =true;
     MotATrouver = std::string("hexakosioihexekontahexaphobie");
     TailleMot = MotATrouver.size();
+    NbLettreATrouver = TailleMot;
+    motTravail = MotATrouver;
+
+    cout << MotATrouver<<endl;
+    cout <<TailleMot<<endl;
+
+    //do{
+        cout <<"Il vous reste "<<TailleMot<<" Lettres a trouver"<<endl;
+        cout <<"Saisir une lettre :";
+        cin >> LettreUser;
+        cout <<LettreUser<<endl;
+
+        for(int i=0;i<=25;i++)	{
+			if(lettresAEssayer[i] == LettreUser){
+
+                    lettreDejaEssayee = false;
+                    lettresAEssayer[i] = '_';
+                    cout<<lettresAEssayer;
+			}
+
+		}
+		if(lettreDejaEssayee == false){
+            bool lettreEstTrouver = false;
+            for(int i = 0; i<TailleMot; i++){
+                if(MotATrouver[i] == LettreUser){
+                    lettreEstTrouver = true;
+                    NbLettreATrouver--;
+                }
+            }
+        if (lettreEstTrouver == false){
+                nbErreur++;
+        }
 
 
-    cout <<"Il vous reste "<<TailleMot<<" Lettres a trouver"<<endl;
-    cout <<"Saisir une lettre :";
-    cin >> lettreAEssayer;
+		}
+
+
+
+//		if (!LettreUser){
+//            bool lettreEstTrouver=false;
+//            for(int i = 0; i<TailleMot;i++){
+//                if (MotATrouver[i]== LettreUser){
+//                    lettreEstTrouver = true;
+//                    motTravail[i*2] = MotATrouver[i];
+//                    TailleMot--;
+//                }
+//            }
+//            if(lettreEstTrouver){
+//                nbErreur++;
+//            }
+//            for(int j=0; j<=25;j++)	{
+//				if(lettresAEssayer[j] == LettreUser)
+//					lettresAEssayer[j] = '_';
+//			}
+		}
+
+    //}
 
 
 
@@ -30,9 +84,6 @@ int main()
 
 
 
-
-
-
-}
+//}
 
 
