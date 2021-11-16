d4d484ae4fa5186cb9c67dc369ba0f48d3d3bf73
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2021-11-16 16:05:38 +0100

    ajout boucle afficher le mot caché

--- JeuDuPendu/main.cpp
+++ JeuDuPendu/main.cpp
@@ -13,77 +13,70 @@ int main()
 
     cout << "Bienvenue dans le jeu du pendu !"<<endl;
 
-
     int TailleMot(0), nbErreur(0), NbLettreATrouver(0);
-    string MotATrouver, motTravail;
+    string MotATrouver, motCacher;
     char LettreUser;
     bool lettreDejaEssayee =true;
+    bool lettreEstTrouver = false;
     MotATrouver = std::string("hexakosioihexekontahexaphobie");
     TailleMot = MotATrouver.size();
     NbLettreATrouver = TailleMot;
-    motTravail = MotATrouver;
+    motCacher = MotATrouver;
+    for(int cpt=0; cpt<(TailleMot*2); cpt=cpt+2)	{
+		motCacher[cpt] = '_';
+		motCacher[cpt+1] = '_';
+	}
 
     cout << MotATrouver<<endl;
     cout <<TailleMot<<endl;
 
-    //do{
+
+        cout <<"Mot a chercher :"<<motCacher<<endl;
         cout <<"Il vous reste "<<TailleMot<<" Lettres a trouver"<<endl;
         cout <<"Saisir une lettre :";
         cin >> LettreUser;
         cout <<LettreUser<<endl;
 
-        for(int i=0;i<=25;i++)	{
+        for(int i=0;i<=25;i++)	{ // parcours la tableau des lettres utilisable
 			if(lettresAEssayer[i] == LettreUser){
 
-                    lettreDejaEssayee = false;
-                    lettresAEssayer[i] = '_';
-                    cout<<lettresAEssayer;
+                lettreDejaEssayee = false;
+                lettresAEssayer[i] = '_';
+                //cout<<lettresAEssayer;
 			}
 
+
 		}
-		if(lettreDejaEssayee == false){
-            bool lettreEstTrouver = false;
-            for(int i = 0; i<TailleMot; i++){
+		if(lettreDejaEssayee == false){ //si la lettre n'est pas deja essayer
+            for(int i = 0; i<TailleMot; i++){ //
                 if(MotATrouver[i] == LettreUser){
                     lettreEstTrouver = true;
-                    NbLettreATrouver--;
+                    NbLettreATrouver--; //reduit de le nombre de lettre a trouver
                 }
             }
-        if (lettreEstTrouver == false){
-                nbErreur++;
-        }
+            if (lettreEstTrouver == false){
+                    nbErreur++;
+            }
+            if (nbErreur == 6 ){
+                cout<<"C'est perdu !";
+                cout<<"La reponse etait :"<<MotATrouver<<"."<<endl;
+            }
+            if (NbLettreATrouver == 0){
+                cout<<"C'est gagner";
+            }
 
 
 		}
-
-
-
-//		if (!LettreUser){
-//            bool lettreEstTrouver=false;
-//            for(int i = 0; i<TailleMot;i++){
-//                if (MotATrouver[i]== LettreUser){
-//                    lettreEstTrouver = true;
-//                    motTravail[i*2] = MotATrouver[i];
-//                    TailleMot--;
-//                }
-//            }
-//            if(lettreEstTrouver){
-//                nbErreur++;
-//            }
-//            for(int j=0; j<=25;j++)	{
-//				if(lettresAEssayer[j] == LettreUser)
-//					lettresAEssayer[j] = '_';
-//			}
+		else{
+            cout << "Vous avez deja essaye cette lettre !";
 		}
 
-    //}
+}
 
 
 
 
 
 
-
-//}
 
 
