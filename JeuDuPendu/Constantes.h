ee585bee5f72b8fb8f1e6151570bf059c87f87f5
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2021-11-17 09:57:17 +0100

    ajout mot aleatoire

--- JeuDuPendu/Constantes.h
+++ JeuDuPendu/Constantes.h
@@ -5,4 +5,10 @@
 char lettresAEssayer[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
+const auto MOT1{"oui"};
+const auto MOT2{"non"};
+const auto MOT3{"test"};
+const auto MOT4{"ok"};
+
+
 #endif // CONSTANTES_H_INCLUDED
